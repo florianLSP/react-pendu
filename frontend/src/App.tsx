750d110ec8844713b9a87cb832d245ba2b504ac8
flsp <florian.lesp@gmail.com>
2025-06-09 12:31:50 +0200

    feat: display the game keyboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,9 @@ function App() {
 
       <HangmanDrawing />
       <HangmanWord />
-      <Keyboard />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
     </div>
   );
 }
